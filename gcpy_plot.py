59728d47436df44614f8c0fa860381090a26a30d
Jordan Sun <sk.jordan@foxmail.com>
2025-06-23 16:39:15 -0500

    Update size of plot and title.

--- gcpy_plot.py
+++ gcpy_plot.py
@@ -5,8 +5,8 @@ import cartopy.crs as ccrs
 import gcpy
 
 # Relative path to the dataset
-resolution = 180
-processors = 576
+resolution = 24
+processors = 144
 type = f"c{resolution}_p{processors}"
 file = "GEOSChem.KppDiags.20190701_0000z.nc4"
 
@@ -18,6 +18,7 @@ os.makedirs(f"figures/{type}", exist_ok=True)
 kpp_sum = ds["KppTotSteps"].sum(dim="lev")
 
 # Plotting with gcpy (summed over levels)
+plt.figure(figsize=(10,9))
 gcpy.plot.single_panel(
     kpp_sum,
     title=f"C{resolution} Global Column Total KPP Steps",
@@ -25,15 +26,15 @@ gcpy.plot.single_panel(
 )
 plt.savefig(f"figures/{type}/gcpy_sum_{file}.pdf", bbox_inches="tight")
 
-# Plotting with Cartopy (summed over levels)
-plt.figure()
-ax = plt.axes(projection=ccrs.EqualEarth())
-ax.coastlines()
-ax.set_global()
+# # Plotting with Cartopy (summed over levels)
+# plt.figure()
+# ax = plt.axes(projection=ccrs.EqualEarth())
+# ax.coastlines()
+# ax.set_global()
 
-for face in range(6):
-    x = ds.corner_lons.isel(nf=face)
-    y = ds.corner_lats.isel(nf=face)
-    v = kpp_sum.isel(time=0, nf=face)  # summed over lev
-    ax.pcolormesh(x, y, v, transform=ccrs.PlateCarree())
-plt.savefig(f"figures/{type}/cartopy_sum_{file}.pdf", bbox_inches="tight")
+# for face in range(6):
+#     x = ds.corner_lons.isel(nf=face)
+#     y = ds.corner_lats.isel(nf=face)
+#     v = kpp_sum.isel(time=0, nf=face)  # summed over lev
+#     ax.pcolormesh(x, y, v, transform=ccrs.PlateCarree())
+# plt.savefig(f"figures/{type}/cartopy_sum_{file}.pdf", bbox_inches="tight")
